4eb95e4dc90132b3ba9f5f313476ab12ed7e8639
PranjalAgni <pranjal.agnihotri6@gmail.com>
2021-09-17 23:38:27 +0530

    :sparkles: trie implementation + longest common prefix

---
+++ trie-implementation.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TrieNode {
+	struct TrieNode* children[26];
+	bool isLeaf;
+};
+
+
+struct TrieNode* getNode() {
+	struct TrieNode* currentNode = new TrieNode();
+	currentNode->isLeaf = false;
+
+	for (int idx = 0; idx < 26; idx++) {
+		currentNode->children[idx] = NULL;
+	}
+
+	return currentNode;
+}
+
+void insert(struct TrieNode* root, string key) {
+
+	struct TrieNode* runnerNode = root;
+	for (char ch : key) {
+		int index = ch - 'a';
+		if (!runnerNode->children[index]) {
+			runnerNode->children[index] = getNode();
+		}
+
+		runnerNode = runnerNode->children[index];
+	}
+
+	runnerNode->isLeaf = true;
+}
+
+bool search(struct TrieNode* root, string key) {
+	struct TrieNode* runnerNode = root;
+	for (char ch : key) {
+		int index = ch - 'a';
+		if (!runnerNode->children[index]) return false;
+
+		runnerNode = runnerNode->children[index];
+	}
+
+	return runnerNode->isLeaf;
+}
+
+int countChildren(struct TrieNode* node, int& index) {
+	int count = 0;
+	for (int idx = 0; idx < 26; idx++) {
+		if (node->children[idx]) {
+			count += 1;
+			index = idx;
+		}
+	}
+
+	return count;
+}
+
+string longestCommonPrefix(struct TrieNode* root, char start) {
+	string longestPrefix = "";
+	struct TrieNode* runnerNode = root;
+	int index;
+	while (countChildren(runnerNode, index) == 1 && !runnerNode->isLeaf) {
+		runnerNode = runnerNode->children[index];
+		longestPrefix.push_back('a' + index);
+	}
+
+	return longestPrefix;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+	// for getting input from input.txt
+	freopen("input.txt", "r", stdin);
+	// for writing output to output.txt
+	freopen("output.txt", "w", stdout);
+#endif
+
+	vector<string> words = {"thisisbeautiful", "thisis", "thisisupper", "thisisbestnews", "thisisit"};
+	struct TrieNode* root = getNode();
+	for (string word : words) {
+		insert(root, word);
+	}
+
+	cout << longestCommonPrefix(root, 't') << endl;
+	return 0;
+}
