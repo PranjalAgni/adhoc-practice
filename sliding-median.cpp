a641442134be6aa00bf3fcfdedbe2ce583da61d8
PranjalAgni <pranjal.agnihotri6@gmail.com>
2021-11-02 19:40:39 +0530

    :sunflower: solved sliding median problem

--- sliding-median.cpp
+++ sliding-median.cpp
@@ -1,6 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void removeElementFromSet(set<pair<int, int>>& leftSet, set<pair<int, int>>& rightSet, int elt, int targetIdx) {
+	set<pair<int, int>>::iterator it = leftSet.find(make_pair(elt, targetIdx));
+	if (it != leftSet.end()) {
+		leftSet.erase(it);
+		return;
+	}
+	it = rightSet.find(make_pair(elt, targetIdx));
+	rightSet.erase(it);
+	return;
+}
+
+void balanceSet(set<pair<int, int>>& leftSet, set<pair<int, int>>& rightSet, int& L, int& R) {
+	while ((int)leftSet.size() > L) {
+		set<pair<int, int>>::iterator it = leftSet.end();
+		--it;
+		leftSet.erase(it);
+		rightSet.insert(make_pair(it->first, it->second));
+	}
+
+
+	while ((int)rightSet.size() > R) {
+		set<pair<int, int>>::iterator it = rightSet.begin();
+		leftSet.insert(make_pair(it->first, it->second));
+		rightSet.erase(it);
+	}
+
+	return;
+}
+
+
+int decideWhereToInsert(set<pair<int, int>>& leftSet, set<pair<int, int>>& rightSet, int elt) {
+	set<pair<int, int>>::iterator it;
+	if (!leftSet.empty()) {
+		it = leftSet.end();
+		--it;
+		if (it->first >= elt) return 1;
+	}
+
+	return 2;
+}
+
 int main() {
 #ifndef ONLINE_JUDGE
 	// for getting input from input.txt
@@ -12,43 +53,31 @@ int main() {
 	int N, K;
 	cin >> N >> K;
 
-	vector<int> elements(N);
-	for (int idx = 0; idx < N; idx++) cin >> elements[idx];
+	vector<int> nums(N);
 
-	int pos = 0;
-	vector<int> nums;
-	vector<int> medianOfWindow;
-	set<pair<int, int>> window;
-	// 1. If length is odd
-	// get middle element from set if
-	// else get the lower middle element from the set
-	// 2. remove the pos index element from set
-	// 3. add the median to answer list
-
-	int medianElementIndex = (K % 2 == 0) ? K / 2 - 1 : K / 2;
-
-	while (pos < N) {
-		pair<int, int> p = make_pair(nums[pos], pos);
-		window.insert(p);
-		cout << window.size() << endl;
-		int windowSize = window.size();
-		if (windowSize == K) {
-			int currentIndex = 0;
-
-			for (const auto& iterator : window) {
-				if (medianElementIndex == currentIndex) {
-					medianOfWindow.push_back(iterator.first);
-					break;
-				}
-				currentIndex += 1;
-			}
-
-			// remove the element from set
-			window.erase({nums[pos - K], K});
-		}
-		pos += 1;
+	for (int idx = 0; idx < N; idx++) {
+		cin >> nums[idx];
 	}
 
-	for (int median : medianOfWindow) cout << median << " ";
+	set<pair<int, int>> leftSet, rightSet;
+	set<pair<int, int>>::iterator it;
+	int L = (K % 2 == 0) ?  (K / 2) - 1 : K / 2;
+	int R = K - L;
+
+	for (int idx = 0; idx < N; idx++) {
+		int setIdToInsert = decideWhereToInsert(leftSet, rightSet, nums[idx]);
+		if (setIdToInsert == 1) leftSet.insert(make_pair(nums[idx], idx));
+		else rightSet.insert(make_pair(nums[idx], idx));
+
+		balanceSet(leftSet, rightSet, L, R);
+		if (idx >= K - 1) {
+			it = rightSet.begin();
+			cout << it->first << " ";
+			removeElementFromSet(leftSet, rightSet, nums[idx - (K - 1)], idx - (K - 1));
+		}
+
+	}
+
+	cout << endl;
 	return 0;
 }
