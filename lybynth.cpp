c2d77039a2f25cda7e69c7696b8a0a3c46730db2
PranjalAgni <pranjal.agnihotri6@gmail.com>
2021-11-16 14:06:18 +0530

    :rocket: accepted: optimized time complexity

--- lybynth.cpp
+++ lybynth.cpp
@@ -1,11 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct NodeInfo {
-	vector<char> path;
-	int row;
-	int col;
-};
 
 bool isValid(int& N, int& M, int row, int col) {
 	return (row >= 0 && row < N && col >= 0 && col < M);
@@ -18,48 +13,53 @@ int main() {
 	// for writing output to output.txt
 	freopen("output.txt", "w", stdout);
 #endif
+	clock_t startTime = clock();
 
 	int N, M;
 	cin >> N >> M;
 	vector<vector<char>> graph(N, vector<char>(M));
-	queue<NodeInfo> nodeQueue;
+	vector<vector<bool>> visited(N, vector<bool>(M, false));
 
+	queue<pair<int, int>> nodeQueue;
+	pair<int, int> begin, end;
 	for (int row = 0; row < N; row++) {
 		for (int col = 0; col < M; col++) {
 			char ch;
 			cin >> ch;
 			graph[row][col] = ch;
 			if (ch == 'A') {
-				NodeInfo nodeInfo = {{}, row, col};
-				nodeQueue.push(nodeInfo);
+				begin = {row, col};
+				nodeQueue.push(begin);
+				visited[row][col] = true;
+			} else if (ch == 'B') {
+				end = {row, col};
+			} else if (ch == '#') {
+				visited[row][col] = true;
 			}
 		}
 	}
 
-	string finalPath = "";
 	bool isReached = false;
-
+	map<pair<int, int>, char> parent;
+	map<char, pair<int, int>> directionsMap = {{'R', {0, 1}}, {'U', { -1, 0}}, {'L', {0, -1}}, {'D', {1, 0}}};
 	while (!nodeQueue.empty()) {
-		NodeInfo nodeInfo = nodeQueue.front();
+		pair<int, int> currentNode = nodeQueue.front();
 		nodeQueue.pop();
-		vector<pair<int, pair<int, char>>> directions = {{0, {1, 'R'}}, {0, { -1, 'L'}}, {1, {0, 'D'}}, { -1, {0, 'U'}}};
 
-		for (pair<int, pair<int, char>> dir : directions) {
-			int newRow = nodeInfo.row + dir.first;
-			int newCol = nodeInfo.col + dir.second.first;
 
-			if (isValid(N, M, newRow, newCol)) {
+		for (const auto &[direction, stepValue] : directionsMap) {
+			int newRow = currentNode.first + stepValue.first;
+			int newCol = currentNode.second + stepValue.second;
+
+			if (isValid(N, M, newRow, newCol) && !visited[newRow][newCol]) {
 				char ch = graph[newRow][newCol];
+				visited[newRow][newCol] = true;
+				pair<int, int> nextNodeInfo = {newRow, newCol};
+				parent[nextNodeInfo] = direction;
 				if (ch == 'B') {
-					nodeInfo.path.push_back(dir.second.second);
-					finalPath = string(nodeInfo.path.begin(), nodeInfo.path.end());
 					isReached = true;
 					break;
 				} else if (ch == '.') {
-					graph[newRow][newCol] = '#';
-					vector<char> currentPath = nodeInfo.path;
-					currentPath.push_back(dir.second.second);
-					NodeInfo nextNodeInfo = {currentPath, newRow, newCol};
 					nodeQueue.push(nextNodeInfo);
 				}
 			}
@@ -70,11 +70,22 @@ int main() {
 
 	if (isReached) {
 		cout << "YES" << endl;
-		cout << finalPath.length() << endl;
-		cout << finalPath << endl;
+		vector<char> steps;
+		while (end != begin) {
+			char step = parent[ {end.first, end.second}];
+			steps.push_back(step);
+			pair<int, int> direction = directionsMap[step];
+			end = make_pair(end.first - direction.first, end.second - direction.second);
+		}
+		cout << steps.size() << endl;
+		for (int idx = steps.size() - 1; idx >= 0; idx--) cout << steps[idx];
 	} else {
 		cout << "NO" << endl;
 	}
 
+	clock_t endTime = clock();
+	double elapsed_secs = double(endTime - startTime) / CLOCKS_PER_SEC;
+
+	cout << "\nClock time = " << elapsed_secs << "sec" << endl;
 	return 0;
 }
