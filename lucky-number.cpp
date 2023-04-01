9c33e173e8e53ef0ac71d04b67028357414a8d7d
PranjalAgni <pranjal.agnihotri6@gmail.com>
2023-04-01 23:53:30 +0530

    :sparkles: using string version to solve lucky number

--- lucky-number.cpp
+++ lucky-number.cpp
@@ -22,11 +22,11 @@ int main() {
 	freopen("output.txt", "w", stdout);
 #endif
 
-	vector<int> luckiness(1000 + 1);
+	// vector<int> luckiness(1000 + 1);
 
-	for (int i = 1; i <= 1000; i++) {
-		luckiness[i] = getLuckiness(i);
-	}
+	// for (int i = 1; i <= 1000; i++) {
+	// 	luckiness[i] = getLuckiness(i);
+	// }
 
 	int T;
 	cin >> T;
@@ -34,14 +34,18 @@ int main() {
 	while (T-- > 0) {
 		int L, R;
 		cin >> L >> R;
-		string numStr = "901";
 
-		int start = max(L, R - 100);
-		int answer = start;
-		for (int idx = start; idx <= R; idx++) {
-			if (luckiness[idx] >= luckiness[answer]) {
+		int answer = L;
+		int luckiness = INT_MIN;
+		for (int idx = L; idx <= R; idx++) {
+			string digits = to_string(idx);
+			int max_dig = *max_element(digits.begin(), digits.end());
+			int min_dig = *min_element(digits.begin(), digits.end());
+			if (max_dig - min_dig >= luckiness) {
+				luckiness = max_dig - min_dig;
 				answer = idx;
 			}
+
 		}
 
 		cout << answer;
@@ -52,10 +56,6 @@ int main() {
 
 	return 0;
 }
-
-
-
-
 
 // Rent  Room    1m  6d   24d
 // 16000 pranjal 534 3204 12816(6408*2)
